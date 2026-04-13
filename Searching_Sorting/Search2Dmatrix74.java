e37840520000e028d7c73e733386ec799162c794
sahil kumar <sahilkumar06804@gmail.com>
2026-04-13 18:29:39 +0530

    2problem

---
+++ Searching_Sorting/Search2Dmatrix74.java
@@ -0,0 +1,34 @@
+package data_structure_and_algorithem.Searching_Sorting;
+
+public class Search2Dmatrix74 {
+    class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int left = 0;
+        int right = m*n -1;
+
+        while(left<=right){
+            int mid = left + (right - left)/2;
+            int midValue = matrix[mid / n][mid % n];
+
+            if(midValue == target){
+                return true;
+            }
+            else if(midValue < target){
+                left = mid + 1;
+
+            }
+            else {
+                right = mid -1;
+            }
+
+        }
+        return false;
+
+        
+    }
+}
+    
+}
