18c75ff8399fd1be1179cbf2d737758c1711f40d
sahil kumar <sahilkumar06804@gmail.com>
2026-04-17 21:25:20 +0530

    kal 2midterm hai

---
+++ Linked-List/reverseList206.java
@@ -0,0 +1,30 @@
+package data_structure_and_algorithem.Linked-List;
+
+public class reverseList206 {
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while(curr!= null){
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+        
+    }
+}
+    
+}
