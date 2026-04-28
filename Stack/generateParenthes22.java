3adb456f6aee936a219ce6fcd6db6fd5d2a5e9e2
sahil kumar <sahilkumar06804@gmail.com>
2026-04-28 22:53:59 +0530

    1problem

---
+++ Stack/generateParenthes22.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class generateParenthes22 {
+    class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList();
+        backTrack(ans, new StringBuilder(), 0, 0, n);
+        return ans;
+        
+    }
+
+    public void backTrack(List<String> ans, StringBuilder curr, int open, int close, int max){
+        if(curr.length() == max*2){
+            ans.add(curr.toString());
+            return;
+        }
+        if(open< max){
+            curr.append("(");
+            backTrack(ans, curr, open+1, close, max);
+            curr.deleteCharAt(curr.length()-1);
+        }
+        if(close<open){
+            curr.append(")");
+            backTrack(ans, curr, open, close+1, max);
+            curr.deleteCharAt(curr.length()-1);
+
+        }
+    }
+}
+    
+}
