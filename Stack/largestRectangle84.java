d9d844267dd274cd48d97a381e6ee67ece9e3108
sahil kumar <sahilkumar06804@gmail.com>
2026-04-30 22:54:10 +0530

    kal mefa ka exam hai or kuch nhi aata hai

---
+++ Stack/largestRectangle84.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+public class largestRectangle84 {
+    class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int maxArea = 0;
+        Stack<Integer> stack = new Stack<>();
+        int n = heights.length;
+
+        for(int i = 0; i<=n; i++){
+            int currentHeight = (i == n) ? 0 : heights[i];
+            while(!stack.isEmpty() && currentHeight < heights[stack.peek()]){
+                int height = heights[stack.pop()];
+                int width = stack.isEmpty() ? i : i - stack.peek() -1;
+                int area = height * width;
+                maxArea = Math.max(maxArea, area);
+            }
+            stack.push(i);
+
+        }
+        return maxArea;
+        
+    }
+}
+    
+}
